2e78c4e54db3d3d9997c8a41d76e40f0f8497ae3
Lius <morarolivio@gmail.com>
2024-05-24 20:15:52 +0200

    bruh

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+from data.config import WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_HOST, IP
+from loader import dp, bot, SSL_CERTIFICATE, ssl_context
+
+
 async def on_startup(dp):
+    # set webhook
+    await bot.set_webhook(
+        url=WEBHOOK_URL,
+        certificate=SSL_CERTIFICATE
+    )
+
     import filters
     import middlewares
     filters.setup(dp)
@@ -10,8 +20,17 @@ async def on_startup(dp):
     from utils.set_bot_commands import set_default_commands
     await set_default_commands(dp)
 
+
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    executor.start_polling(dp, on_startup=on_startup)
+    # executor.start_polling(dp, on_startup=on_startup)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+        ssl_context=ssl_context
+    )
